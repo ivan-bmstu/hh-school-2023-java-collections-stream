17a1f1f28bfe53ddbb9d7d66171cabb03a18542b
Ivan Shirokov <laefree@yandex.ru>
2023-11-20 13:33:08 +0300

    solved task6 with streamAPI

--- src/main/java/tasks/Task6.java
+++ src/main/java/tasks/Task6.java
@@ -6,6 +6,7 @@ import java.util.Collection;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /*
 Имеются
@@ -19,6 +20,14 @@ public class Task6 {
   public static Set<String> getPersonDescriptions(Collection<Person> persons,
                                                   Map<Integer, Set<Integer>> personAreaIds,
                                                   Collection<Area> areas) {
-    return new HashSet<>();
+    Map<Integer, String> idAreaName = areas.stream().collect(Collectors.toMap(Area::getId, Area::getName));
+    return persons.stream()
+            .flatMap(person -> personAreaIds.get(person.getId()).stream()
+                    .map(id ->
+                            person
+                                    .getFirstName()
+                                    + " - "
+                                    + idAreaName.get(id)))
+            .collect(Collectors.toSet());
   }
 }
