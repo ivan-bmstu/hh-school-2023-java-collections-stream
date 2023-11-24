1656ef9bce4073c59fe0a94fd2bb53e155282cd3
Ivan Shirokov <laefree@yandex.ru>
2023-11-24 15:12:39 +0300

    code style in task8

--- src/main/java/tasks/Task8.java
+++ src/main/java/tasks/Task8.java
@@ -1,12 +1,8 @@
 package tasks;
 
 import common.Person;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -21,67 +17,85 @@ P.P.S Здесь ваши правки желательно прокоммент
  */
 public class Task8 {
 
-  private long count;
 
   //Не хотим выдывать апи нашу фальшивую персону, поэтому конвертим начиная со второй
-  public List<String> getNames(List<Person> persons) {
-    if (persons.size() == 0) {
+  public List<String> getNamesExceptFirst(List<Person> persons) {
+    if (persons.isEmpty()) {
       return Collections.emptyList();
     }
-    persons.remove(0);
-    return persons.stream().map(Person::getFirstName).collect(Collectors.toList());
+    return persons.stream()
+            .skip(1)
+            .map(Person::getFirstName)
+            .collect(Collectors.toList());
   }
+  /*
+  * -изменил имя метода, теперь оно лучше отображает его функциональность. Но тут
+  * нужно обсуждать этот момент с командой: во-первых, этот метод мог вызываться во многих местах,
+  * и заменить имя его может быть проблемно. Более того, возможно так и планировалось оставить часть функционала скрытой
+  * от пользователей, чтобы не были ясны некоторые "нужные" рабочие моменты. Это изменение обсуждаемо
+  *
+  * -убрал первую фейковую персону непосредственно в stream, меньше кода, лучше читаемость
+  *
+  * -немного подправил code style в stream, перенес действия построчно
+  *
+  * -я бы удалил комментарий перед методом, но сначала обсудил бы с командой (иначе можно и забыть, зачем убирать
+  * первую персону в принципе)
+  *
+  * -заменил persons.size() == 0 на persons.isEmpty()
+  * */
 
-  //ну и различные имена тоже хочется
-  public Set<String> getDifferentNames(List<Person> persons) {
-    return getNames(persons).stream().distinct().collect(Collectors.toSet());
+  public Set<String> getDifferentNamesExceptFirst(List<Person> persons) {
+    return getNamesExceptFirst(persons).stream().collect(Collectors.toSet());
   }
+  /*
+  * -убрал комментарий перед методом, имя метода полностью отображает его функциональность
+  *
+  * -вызов метода stream.distinct() избыточен, т.к. Set возвращает только уникальные значения.
+  * p.s. еле удержал себя, чтобы не изменить код: сделать все как в методе getNamesExceptFirst,
+  * но вернуть не List, а Set. Это небольшая оптимизация, но тут будет повторение кода (проверка на пустоту)
+  *
+  * -имя метода поменял, причина и описание как и выше в методе getNamesExceptFirst
+  *
+  * p.p.s. так и не понял, есть ли преимущества по производительности, если здесь использовать не stream, а конструктор
+  * new HashSet<>(getNamesExceptFirst(persons))
+  * */
 
   //Для фронтов выдадим полное имя, а то сами не могут
   public String convertPersonToString(Person person) {
-    String result = "";
-    if (person.getSecondName() != null) {
-      result += person.getSecondName();
-    }
-
-    if (person.getFirstName() != null) {
-      result += " " + person.getFirstName();
-    }
-
-    if (person.getSecondName() != null) {
-      result += " " + person.getSecondName();
-    }
-    return result;
+    return Stream.of(person.getSecondName(),
+                    person.getFirstName(),
+                    person.getSecondName())
+            .filter(Objects::nonNull)
+            .collect(Collectors.joining(" "));
   }
+  /*
+  * -переписал код на stream. Это удобнее: легче менять и расширять код (например последовательность, или добавить
+  * дополнительные поля)
+  *
+  * -выводится не ФИО, а ФИФ - тут непонятно, так и должно быть, или нужно править, поэтому обсуждаем с командой. Сейчас
+  * оставил исходный вариант
+  *
+  * */
 
-  // словарь id персоны -> ее имя
   public Map<Integer, String> getPersonNames(Collection<Person> persons) {
-    Map<Integer, String> map = new HashMap<>(1);
-    for (Person person : persons) {
-      if (!map.containsKey(person.getId())) {
-        map.put(person.getId(), convertPersonToString(person));
-      }
-    }
-    return map;
+    return persons.stream()
+            .collect(Collectors.toMap(Person::getId, this::convertPersonToString, (p1, p2) -> p1));
   }
+  /*
+  * -переделал метод через stream, стало меньше кода, код читаемее
+  * */
 
-  // есть ли совпадающие в двух коллекциях персоны?
   public boolean hasSamePersons(Collection<Person> persons1, Collection<Person> persons2) {
-    boolean has = false;
-    for (Person person1 : persons1) {
-      for (Person person2 : persons2) {
-        if (person1.equals(person2)) {
-          has = true;
-        }
-      }
-    }
-    return has;
+    return !Collections.disjoint(persons1, persons2);
   }
+  /*
+  * -воспользовался функционалом Collections
+  * */
 
-  //...
   public long countEven(Stream<Integer> numbers) {
-    count = 0;
-    numbers.filter(num -> num % 2 == 0).forEach(num -> count++);
-    return count;
+    return numbers.filter(num -> num % 2 == 0).count();
   }
+  /*
+  * убрал поле count, оно здесь избыточно
+  * */
 }
